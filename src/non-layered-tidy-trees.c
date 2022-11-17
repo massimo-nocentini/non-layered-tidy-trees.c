416c75e9cdb3b2e479d76a9962c7e2599dd0e9da
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-11-17 13:02:53 +0100

    Cleaning

--- src/non-layered-tidy-trees.c
+++ src/non-layered-tidy-trees.c
@@ -21,18 +21,18 @@ static IYL_t * updateIYL(double minY, int i, IYL_t *init) {
 static void addChildSpacing(tree_t *t){
   double d = 0.0, modsumdelta = 0.0;
   for(int i = 0 ; i < t->cs; i++){
-    d+=t->c[i]->shift;
-    modsumdelta+=d + t->c[i]->change;
-    t->c[i]->mod+=modsumdelta;
+    d += t->c[i]->shift;
+    modsumdelta += d + t->c[i]->change;
+    t->c[i]->mod += modsumdelta;
   }
 }
 
 static double bottom(tree_t *t) { return t->y + t->h;  }
 
-static  void setExtremes(tree_t *t) {
+static void setExtremes(tree_t *t) {
   if(t->cs == 0){
     t->el = t; t->er = t;
-    t->msel = t->mser =0.0;
+    t->msel = t->mser = 0.0;
   } else {
     t->el = t->c[0]->el; t->msel = t->c[0]->msel;
     t->er = t->c[t->cs-1]->er; t->mser = t->c[t->cs-1]->mser;
@@ -43,9 +43,10 @@ static void distributeExtra(tree_t *t, int i, int si, double dist) {
   // ^{\normalfont Are there intermediate children?}^
   if(si != i-1){
     double nr = i - si;
-    t->c[si +1]->shift+=dist/nr;
-    t->c[i]->shift-=dist/nr;
-    t->c[i]->change-=dist - dist/nr;
+    double ratio = dist / nr;
+    t->c[si + 1]->shift += ratio;
+    t->c[i]->shift -= ratio;
+    t->c[i]->change -= dist - ratio;
   }
 }
 
@@ -59,28 +60,28 @@ static tree_t * nextLeftContour (tree_t *t) {return t->cs==0 ? t->tl : t->c[0];}
 static tree_t * nextRightContour(tree_t *t) {return t->cs==0 ? t->tr : t->c[t->cs-1];}
 
 static void setLeftThread(tree_t *   t, int i, tree_t *     cl, double modsumcl) {
-  tree_t *         li = t->c[0]->el;
+  tree_t *li = t->c[0]->el;
   li->tl = cl;
   // ^{\normalfont Change mod so that the sum of modifier after following thread is correct.}^
   double diff = (modsumcl - cl->mod) - t->c[0]->msel ;
   li->mod += diff;
   // ^{\normalfont Change preliminary x coordinate so that the node does not move.}^
-  li->prelim-=diff;
+  li->prelim -= diff;
   // ^{\normalfont Update extreme node and its sum of modifiers.}^
   t->c[0]->el = t->c[i]->el; t->c[0]->msel = t->c[i]->msel;
 }
 
 // ^{\normalfont Symmetrical to setLeftThread.}^
 static void setRightThread(tree_t *     t, int i, tree_t *     sr, double modsumsr) {
-  tree_t *         ri = t->c[i]->er;
+  tree_t *ri = t->c[i]->er;
   ri->tr = sr;
   double diff = (modsumsr - sr->mod) - t->c[i]->mser ;
   ri->mod += diff;
-  ri->prelim-=diff;
+  ri->prelim -= diff;
   t->c[i]->er = t->c[i-1]->er; t->c[i]->mser = t->c[i-1]->mser;
 }
 
-static void seperate(tree_t *t,int i,  IYL_t *ih ){
+static void seperate(tree_t *t,int i,  IYL_t *init ){
   // ^{\normalfont Right contour node of left siblings and its sum of modfiers.}^
   tree_t *sr = t->c[i-1];
   double mssr = sr->mod;
@@ -89,6 +90,7 @@ static void seperate(tree_t *t,int i,  IYL_t *ih ){
   tree_t *cl = t->c[i];
   double mscl = cl->mod;
 
+  IYL_t *ih = init;
   while(sr != NULL && cl != NULL){
     if(bottom(sr) > ih->lowY) ih = ih->nxt;
 
@@ -116,10 +118,12 @@ static void seperate(tree_t *t,int i,  IYL_t *ih ){
   else if(sr != NULL && cl == NULL) setRightThread(t,i,sr,mssr);
 }
 
-static void positionRoot(tree_t *     t) {
+static void positionRoot(tree_t *t) {
   // ^{\normalfont Position root between children, taking into account their mod.}^
   t->prelim = (t->c[0]->prelim + t->c[0]->mod + t->c[t->cs-1]->mod +
-           t->c[t->cs-1]->prelim +  t->c[t->cs-1]->w)/2 - t->w/2;
+               t->c[t->cs-1]->prelim +  t->c[t->cs-1]->w - t->w);
+  
+  t->prelim /= 2;
 }
 
 static void firstWalk(tree_t *t, void *userdata, callback_t cb) {
@@ -144,8 +148,8 @@ static void firstWalk(tree_t *t, void *userdata, callback_t cb) {
   if (cb != NULL) cb (t, userdata);
 }
 
-static void secondWalk(tree_t *    t, double modsum, void *userdata, callback_t cb) {
-  modsum+=t->mod;
+static void secondWalk(tree_t *    t, double modsum_init, void *userdata, callback_t cb) {
+  double modsum = modsum_init + t->mod;
   // ^{\normalfont Set absolute (non-relative) horizontal coordinate.}^
   t->x = t->prelim + modsum;
   if (cb != NULL) cb (t, userdata);
