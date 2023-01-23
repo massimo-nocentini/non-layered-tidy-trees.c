4d8612ed4f75f13e931d79311c9901507df2ed34
Massimo Nocentini <massimo.nocentini@gmail.com>
2023-01-23 13:57:22 +0100

    Adjust the initial position.

--- src/non-layered-tidy-trees.c
+++ src/non-layered-tidy-trees.c
@@ -7,28 +7,33 @@
 /*
  * A linked list of the indexes of left siblings and their lowest vertical coordinate.
  */
-typedef struct chain_s { 
-  double low; 
-  int index; 
+typedef struct chain_s
+{
+  double low;
+  int index;
   struct chain_s *nxt;
 } chain_t;
 
-static void free_chain (chain_t *c) {
-  if (c->nxt != NULL) free_chain (c->nxt);
-  free (c);
+static void free_chain(chain_t *c)
+{
+  if (c->nxt != NULL)
+    free_chain(c->nxt);
+  free(c);
 }
 
-static chain_t * update_chain (double min, int i, chain_t *init) {
-  
-  chain_t * ih = init;
+static chain_t *update_chain(double min, int i, chain_t *init)
+{
 
-  while(ih != NULL && min >= ih->low) {
+  chain_t *ih = init;
+
+  while (ih != NULL && min >= ih->low)
+  {
     chain_t *nxt = ih->nxt;
-    free (ih);  
+    free(ih);
     ih = nxt;
   }
-  
-  chain_t *out = (chain_t *) malloc (sizeof(chain_t));
+
+  chain_t *out = (chain_t *)malloc(sizeof(chain_t));
   out->low = min;
   out->index = i;
   out->nxt = ih;
@@ -37,33 +42,43 @@ static chain_t * update_chain (double min, int i, chain_t *init) {
 }
 
 // Process change and shift to add intermediate spacing to mod.
-static void addChildSpacing(tree_t *t){
+static void addChildSpacing(tree_t *t)
+{
   double d = 0.0, modsumdelta = 0.0;
-  for(int i = 0 ; i < t->cs; i++){
+  for (int i = 0; i < t->cs; i++)
+  {
     d += t->c[i]->shift;
     modsumdelta += d + t->c[i]->change;
     t->c[i]->mod += modsumdelta;
   }
 }
 
-EXPORT double CallingConvention bottom(tree_t *t, int vertically) { 
-  return vertically != 0 ? 
-          t->y + (t->centeredxy == 1 ? t->h / 2.0 : t->h) : 
-          t->x + (t->centeredxy == 1 ? t->w / 2.0 : t->w);
+EXPORT double CallingConvention bottom(tree_t *t, int vertically)
+{
+  return vertically != 0 ? t->y + (t->centeredxy == 1 ? t->h / 2.0 : t->h) : t->x + (t->centeredxy == 1 ? t->w / 2.0 : t->w);
 }
 
-static void setExtremes(tree_t *t) {
-  if(t->cs == 0){
-    t->el = t; t->er = t;
+static void setExtremes(tree_t *t)
+{
+  if (t->cs == 0)
+  {
+    t->el = t;
+    t->er = t;
     t->msel = t->mser = 0.0;
-  } else {
-    t->el = t->c[0]->el; t->msel = t->c[0]->msel;
-    t->er = t->c[t->cs-1]->er; t->mser = t->c[t->cs-1]->mser;
+  }
+  else
+  {
+    t->el = t->c[0]->el;
+    t->msel = t->c[0]->msel;
+    t->er = t->c[t->cs - 1]->er;
+    t->mser = t->c[t->cs - 1]->mser;
   }
 }
 
-static void distributeExtra(tree_t *t, int i, int si, double dist) {
-  if(si != i-1){ // Are there intermediate children?
+static void distributeExtra(tree_t *t, int i, int si, double dist)
+{
+  if (si != i - 1)
+  { // Are there intermediate children?
     double nr = i - si;
     double ratio = dist / nr;
     t->c[si + 1]->shift += ratio;
@@ -72,45 +87,49 @@ static void distributeExtra(tree_t *t, int i, int si, double dist) {
   }
 }
 
-static void moveSubtree (tree_t *t, int i, int si, double dist) {
-  t->c[i]->mod  += dist;
+static void moveSubtree(tree_t *t, int i, int si, double dist)
+{
+  t->c[i]->mod += dist;
   t->c[i]->msel += dist;
   t->c[i]->mser += dist;
   distributeExtra(t, i, si, dist);
 }
 
-static tree_t * nextLeftContour (tree_t *t) {return t->cs==0 ? t->tl : t->c[0];}
-static tree_t * nextRightContour(tree_t *t) {return t->cs==0 ? t->tr : t->c[t->cs-1];}
+static tree_t *nextLeftContour(tree_t *t) { return t->cs == 0 ? t->tl : t->c[0]; }
+static tree_t *nextRightContour(tree_t *t) { return t->cs == 0 ? t->tr : t->c[t->cs - 1]; }
 
-static void setLeftThread(tree_t *t, int i, tree_t *cl, double modsumcl) {
+static void setLeftThread(tree_t *t, int i, tree_t *cl, double modsumcl)
+{
   tree_t *li = t->c[0]->el;
   li->tl = cl;
-  
+
   // Change mod so that the sum of modifier after following thread is correct.
   double diff = (modsumcl - cl->mod) - t->c[0]->msel;
   li->mod += diff;
-  
+
   // Change preliminary x coordinate so that the node does not move.
   li->prelim -= diff;
-  
+
   // Update extreme node and its sum of modifiers.
-  t->c[0]->el = t->c[i]->el; 
+  t->c[0]->el = t->c[i]->el;
   t->c[0]->msel = t->c[i]->msel;
 }
 
 // Symmetrical to `setLeftThread`.
-static void setRightThread(tree_t *t, int i, tree_t *sr, double modsumsr) {
+static void setRightThread(tree_t *t, int i, tree_t *sr, double modsumsr)
+{
   tree_t *ri = t->c[i]->er;
   ri->tr = sr;
   double diff = (modsumsr - sr->mod) - t->c[i]->mser;
   ri->mod += diff;
   ri->prelim -= diff;
-  t->c[i]->er = t->c[i-1]->er; 
-  t->c[i]->mser = t->c[i-1]->mser;
+  t->c[i]->er = t->c[i - 1]->er;
+  t->c[i]->mser = t->c[i - 1]->mser;
 }
 
-static void separate(treeinput_t * input, tree_t *t, int i, chain_t *init) {
-  
+static void separate(treeinput_t *input, tree_t *t, int i, chain_t *init)
+{
+
   tree_t *sr = t->c[i - 1];
   double mssr = sr->mod;
 
@@ -119,70 +138,87 @@ static void separate(treeinput_t * input, tree_t *t, int i, chain_t *init) {
 
   chain_t *ih = init;
 
-  for (int iterations = 0; sr != NULL && cl != NULL; iterations++){
-    
-    if(bottom(sr, input->vertically) > ih->low) ih = ih->nxt;
-    
+  for (int iterations = 0; sr != NULL && cl != NULL; iterations++)
+  {
+
+    if (bottom(sr, input->vertically) > ih->low)
+      ih = ih->nxt;
+
     // How far to the left of the right side of sr is the left side of cl?
     double srd = input->vertically != 0 ? sr->w : sr->h;
     double dist = (mssr + sr->prelim + srd + sr->margin) - (mscl + cl->prelim);
-    
-    if (dist > 0.0 || (iterations == 0 && dist < 0.0)){
+
+    if (dist > 0.0 || (iterations == 0 && dist < 0.0))
+    {
       mscl += dist;
-      moveSubtree (t,i,ih->index,dist);
+      moveSubtree(t, i, ih->index, dist);
     }
 
-    if (input->cpairscb != NULL) input->cpairscb (sr, cl, dist, input->cpairsud);
+    if (input->cpairscb != NULL)
+      input->cpairscb(sr, cl, dist, input->cpairsud);
 
     double sy = bottom(sr, input->vertically);
     double cy = bottom(cl, input->vertically);
-    
-    if(sy <= cy){
+
+    if (sy <= cy)
+    {
       sr = nextRightContour(sr);
-      if(sr != NULL) mssr += sr->mod;
+      if (sr != NULL)
+        mssr += sr->mod;
     }
 
-    if(sy >= cy){
+    if (sy >= cy)
+    {
       cl = nextLeftContour(cl);
-      if(cl != NULL) mscl += cl->mod;
+      if (cl != NULL)
+        mscl += cl->mod;
     }
   }
-  
+
   // Set threads and update extreme nodes.
   // In the first case, the current subtree must be taller than the left siblings.
-  if(sr == NULL && cl != NULL) setLeftThread(t,i,cl,mscl);
+  if (sr == NULL && cl != NULL)
+    setLeftThread(t, i, cl, mscl);
   // In this case, the left siblings must be taller than the current tree.
-  else if(sr != NULL && cl == NULL) setRightThread(t,i,sr,mssr);
+  else if (sr != NULL && cl == NULL)
+    setRightThread(t, i, sr, mssr);
 }
 
-static void positionRoot(tree_t *t, int vertically) {
+static void positionRoot(tree_t *t, int vertically)
+{
   // Position root between children, taking into account their mod.
-  int last = t->cs-1;
+  int last = t->cs - 1;
   double d = vertically != 0 ? t->c[last]->w - t->w : t->c[last]->h - t->h;
   t->prelim = (t->c[0]->prelim + t->c[0]->mod + t->c[last]->prelim + t->c[last]->mod + d) / 2.0;
 }
 
-static void firstWalk(treeinput_t * input, tree_t * t) {
+static void firstWalk(treeinput_t *input, tree_t *t)
+{
 
-  if(t->cs == 0){ setExtremes(t); }
-  else {
-    
+  if (t->cs == 0)
+  {
+    setExtremes(t);
+  }
+  else
+  {
+
     firstWalk(input, t->c[0]);
 
-    chain_t *ih = update_chain (bottom(t->c[0]->el, input->vertically), 0, NULL);
-      
-    for(int i = 1; i < t->cs; i++){
-      
+    chain_t *ih = update_chain(bottom(t->c[0]->el, input->vertically), 0, NULL);
+
+    for (int i = 1; i < t->cs; i++)
+    {
+
       firstWalk(input, t->c[i]);
-      
+
       double min = bottom(t->c[i]->er, input->vertically);
-      
+
       separate(input, t, i, ih);
-      
-      ih = update_chain (min,i,ih);
+
+      ih = update_chain(min, i, ih);
     }
 
-    free_chain (ih);
+    free_chain(ih);
 
     positionRoot(t, input->vertically);
 
@@ -190,71 +226,102 @@ static void firstWalk(treeinput_t * input, tree_t * t) {
   }
 }
 
-static void secondWalk(treeinput_t * input, tree_t *t, double modsum_init) {
-  
-  double modsum = modsum_init + t->mod;   // keep it for the recursive call at the end.
+static void secondWalk(treeinput_t *input, tree_t *t, double modsum_init)
+{
+
+  double modsum = modsum_init + t->mod; // keep it for the recursive call at the end.
   double d = t->prelim + modsum;
 
   double xoffset, yoffset;
 
-  if (input->centeredxy == 1) {
+  if (input->centeredxy == 1)
+  {
     xoffset = t->w / 2.0;
     yoffset = t->h / 2.0;
     t->centeredxy = 1;
-  } else {
+  }
+  else
+  {
     xoffset = 0.0;
     yoffset = 0.0;
     t->centeredxy = 0;
   }
 
-  if (input->vertically == 1) {
+  if (input->vertically == 1)
+  {
     t->x = d + xoffset;
     t->y += yoffset;
-  } else {
+  }
+  else
+  {
     t->x += xoffset;
     t->y = d + yoffset;
   }
 
   addChildSpacing(t);
-  
-  if (input->walkcb != NULL) input->walkcb (t, input->walkud);
-  
-  for(int i = 0 ; i < t->cs ; i++) secondWalk(input, t->c[i], modsum);
+
+  if (input->walkcb != NULL)
+    input->walkcb(t, input->walkud);
+
+  for (int i = 0; i < t->cs; i++)
+    secondWalk(input, t->c[i], modsum);
 }
 
-static void setupWalk (treeinput_t * input, tree_t *t, int level) {
+static void setupWalk(treeinput_t *input, tree_t *t, int level)
+{
 
   t->level = level;
-  t->centeredxy = 0;    // initially the algorithm requires top-left wise coordinates.
-  
+  t->centeredxy = 0; // initially the algorithm requires top-left wise coordinates.
+
   int nextlevel = level + 1;
 
-  double b = bottom (t, input->vertically);
-  
-  for(int i = 0; i < t->cs; i++) {
+  double b = bottom(t, input->vertically);
 
+  for (int i = 0; i < t->cs; i++)
+  {
     tree_t *child = t->c[i];
     child->childno = i;
     child->p = t;
-    
-    if (input->vertically == 1) child->y += b; else child->x += b;
-    
-    setupWalk (input, child, nextlevel);
+
+    if (input->vertically == 1)
+      child->y += b;
+    else
+      child->x += b;
+
+    setupWalk(input, child, nextlevel);
   }
 }
 
-EXPORT void CallingConvention layout(treeinput_t * input){
-  setupWalk (input, input->t, 0);
-  firstWalk (input, input->t);
-  secondWalk(input, input->t, input->mod);
+static void thirdWalk(treeinput_t *input, tree_t *t, double dx, double dy)
+{
+  t->x -= dx;
+  t->y -= dy;
+
+  for (int i = 0; i < t->cs; i++)
+    thirdWalk(input, t->c[i], dx, dy);
 }
 
-EXPORT void CallingConvention  free_tree (tree_t *t) {
+EXPORT void CallingConvention layout(treeinput_t *input)
+{
+  setupWalk(input, input->t, 0);
+  firstWalk(input, input->t);
+  secondWalk(input, input->t, 0.0);
 
-	t->el = t->er = t->tl = t->tr = t->p = NULL;    // disconnect some links.
+  double dx, dy;
 
-	for (int i = 0; i < t->cs; i++) free_tree (t->c[i]);
+  dx = input->t->x - input->x;
+  dy = input->t->y - input->y;
 
-	free (t->c);
-	free (t);
+  thirdWalk(input, input->t, dx, dy);
+}
+
+EXPORT void CallingConvention free_tree(tree_t *t)
+{
+  t->el = t->er = t->tl = t->tr = t->p = NULL; // disconnect some links.
+
+  for (int i = 0; i < t->cs; i++)
+    free_tree(t->c[i]);
+
+  free(t->c);
+  free(t);
 }
--- src/non-layered-tidy-trees.h
+++ src/non-layered-tidy-trees.h
@@ -1,59 +1,60 @@
 
 #ifdef _WIN32
-	#define EXPORT __declspec(dllexport)
+#define EXPORT __declspec(dllexport)
 #else
-	#define EXPORT __attribute__((visibility("default")))
+#define EXPORT __attribute__((visibility("default")))
 #endif
 
 #ifdef _WIN32
-	#if defined __LP64__ || defined _WIN64
-		#define CallingConvention __attribute__((ms_abi))
-	#else
-		#define CallingConvention __stdcall
-	#endif
+#if defined __LP64__ || defined _WIN64
+#define CallingConvention __attribute__((ms_abi))
 #else
-	#define CallingConvention
+#define CallingConvention __stdcall
 #endif
-
+#else
+#define CallingConvention
+#endif
 
 /*
  * A type for hierarchies that act as trees.
  */
-typedef struct tree_s {
+typedef struct tree_s
+{
   int idx;
   int level;
   int childno;
-  int centeredxy;               // relates `x` and `y` to `w` and `h`, respectively.
+  int centeredxy; // relates `x` and `y` to `w` and `h`, respectively.
   int isdummy;
-  double w, h, margin;          // Width and height.
+  double w, h, margin; // Width and height.
   double x, y, prelim, mod, shift, change;
   struct tree_s *tl;
-  struct tree_s *tr;            // Left and right thread.
+  struct tree_s *tr; // Left and right thread.
   struct tree_s *el;
-  struct tree_s *er;            // Extreme left and right nodes.
-  double msel, mser;            // Sum of modifiers at the extreme nodes.
-  struct tree_s **c; int cs;    // Array of children and number of children.
-  struct tree_s *p;             // my parent.
+  struct tree_s *er; // Extreme left and right nodes.
+  double msel, mser; // Sum of modifiers at the extreme nodes.
+  struct tree_s **c;
+  int cs;           // Array of children and number of children.
+  struct tree_s *p; // my parent.
 } tree_t;
 
-typedef void (*callback_t) (tree_t *, void *);
+typedef void (*callback_t)(tree_t *, void *);
 
-typedef void (*contourpairs_t) (tree_t *, tree_t *, double, void *);
+typedef void (*contourpairs_t)(tree_t *, tree_t *, double, void *);
 
-typedef struct treeinput_s {
+typedef struct treeinput_s
+{
+  tree_t *t;
 
-  tree_t *t; 
-  
   int vertically;
-  int centeredxy; 
-  
+  int centeredxy;
+
   void *walkud;
   callback_t walkcb;
-  
+
   void *cpairsud;
   contourpairs_t cpairscb;
 
-  double mod;
+  double x, y;
 
 } treeinput_t;
 
@@ -61,4 +62,4 @@ EXPORT void CallingConvention layout(treeinput_t *);
 
 EXPORT double CallingConvention bottom(tree_t *, int);
 
-EXPORT void CallingConvention  free_tree (tree_t *);
+EXPORT void CallingConvention free_tree(tree_t *);
